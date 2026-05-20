d19afdcef8a286c2d88abe2e4a048e9a62449fe8
k-and-k <cnt19011@ict.nitech.ac.jp>
2026-05-21 08:22:09 +0900

    Add nested loops with exit condition using goto
    Implement nested loops with a goto statement to exit.

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+
+    for (i = 0; i <= 10; i++) {
+        for (j = 0; j <= 10; j++) {
+            for (k = 0; k <= 10; k++) {
+
+                if (i == 7 && j == 7 && k == 7) {
+                    goto END;
+                }
+
+                printf("i=%d j=%d k=%d\n", i, j, k);
+            }
+        }
+    }
+
+END:
+    printf("Loop end\n");
+
+    return 0;
+}
